b343b4ec577394a5cff0c03471f0f21a915c7a00
Simon Imbrogno <turnforever.si@gmail.com>
2019-04-15 21:25:27 -0500

    Implement a Trie

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod trie;
+use trie::Trie;
+
 use std::fs;
 use std::env;
 
@@ -9,83 +12,14 @@ fn read_file(path: &std::path::Path) -> String
   return data;
 }
 
-fn find_max_counter(list: &Vec<Counter>) -> (i32, usize)
-{
-  let mut max_indx = 0;
-  let mut max = list[max_indx].count;
-
-  for (indx, entry) in list.iter().enumerate()
-  {
-    if entry.count > max
-    {
-      max = entry.count;
-      max_indx = indx;
-    }
-  }
-
-  return (max, max_indx);
-}
-
-#[derive(Clone, Debug)]
-struct Counter
-{
-  label: char,
-  count: i32,
-}
-
-fn count_chars(string: &String, counters: &mut Vec<Counter>)
-{
-  for c in string.chars()
-  {
-    let as_integer = c as i32;
-    match as_integer
-    {
-      65...90   => counters[(as_integer - 65) as usize].count += 1,
-      97...122  => counters[(as_integer - 97) as usize].count += 1,
-      _         => (), //Do nothin!
-    };
-  }
-}
-
-fn draw_graph(letter_counts: &Vec<Counter>)
-{
-  const MAX_HEIGHT: i32 = 40;
-
-  let (max_count, _) = find_max_counter(&letter_counts);
-  let step = max_count / MAX_HEIGHT;
-
-  let mut row = String::new();
-
-  for row_indx in (0..MAX_HEIGHT).rev()
-  {
-    for counter_indx in 0..26
-    {
-      if counter_indx > 0 { row.push_str("  ") }
-
-      if row_indx > 0
-      {
-        let bar_reaches_row = (letter_counts[counter_indx].count / step) >= row_indx;
-        if bar_reaches_row { row.push_str("███") }
-        else               { row.push_str("   ") }
-      }
-      else
-      {
-        row.push_str(format!(" {} ", letter_counts[counter_indx].label).as_str());
-      }
-    }
-
-    println!("{}", row);
-    row.clear();
-  }
-}
-
 fn main() -> Result<(), Box<std::error::Error>>
 {
+  let mut trie = Trie::new();
+
   let mut path = env::current_dir().unwrap();
   path.push("inputs");
 
-  let mut counters = vec![Counter{ label: ' ', count: 0}; 26];
-  for i in 0..26 { counters[i].label = (65 + i) as u8 as char }
+  let mut unique_word_count = 0;
 
   println!("Reading files...");
   if path.is_dir()
@@ -98,15 +32,18 @@ fn main() -> Result<(), Box<std::error::Error>>
       if !file_path.is_dir()
       {
         let file_contents = read_file(&file_path);
-        count_chars(&file_contents, &mut counters);
 
+        for word in file_contents.split_whitespace()
+        {
+          let string = word.to_string().replace(".", "");
+          if trie.put(string) == true { unique_word_count += 1 };
+        }
       }
     }
   }
 
-  println!();
-  counters.sort_by(|a: &Counter, b: &Counter| b.count.cmp(&a.count));
-  draw_graph(&counters);
+  trie.print_contents();
+  println!("{} unique words.", unique_word_count);
 
   return Ok(());
 }
---
+++ src/trie.rs
@@ -0,0 +1,144 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Trie
+{
+  root: bool,
+  terminal: bool,
+  label: char,
+  next: [Option<Box<Trie>>; 26],
+}
+
+impl fmt::Display for Trie {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{} '{}' -> [", self.terminal, self.label)?;
+
+    let mut first = true;
+    for elem in self.next.iter()
+    {
+      if let Some(sub) = elem
+      {
+        let label = sub.label;
+        if first
+        {
+          first = false;
+          write!(f, "{}", label)?;
+        }
+        else
+        {
+          write!(f, ", {}", label)?;
+        }
+      }
+    }
+
+    return write!(f, "]");
+  }
+}
+
+impl Trie
+{
+  // Constructor (Root)
+  pub fn new() -> Trie {
+    let mut new_trie = Trie::new_branch(' ');
+    new_trie.root = true;
+    return new_trie;
+  }
+
+  // Constructor
+  fn new_branch(label: char) -> Trie {
+    Trie {
+      root: false,
+      terminal: false,
+      label: label,
+      next: Default::default(),
+    }
+  }
+
+  fn get_sub(&mut self, label: char) -> &mut Trie
+  {
+    let as_int = label as u32;
+    let indx = match as_int
+    {
+      97...122  => (as_int - 97) as usize,
+      _         => self.next.len(),
+    };
+
+    // If this incoming char is a crap value, return the parent for a cheeky little skip.
+    // i.e: we begin inserting again from the parent at the next char!
+    if indx == self.next.len() { return self; };
+
+    if let None = self.next[indx]
+    {
+      self.next[indx] = Some(Box::new(Trie::new_branch(label)));
+    }
+
+    return &mut *(self.next[indx].as_mut().unwrap());
+  }
+
+  pub fn put(&mut self, mut key: String) -> bool
+  {
+    if key.len() > 0
+    {
+      let as_int = (&mut key).remove(0) as u8;
+      let first_char = match as_int
+      {
+        65...90   => (as_int + 32) as char,
+        97...122  => as_int as char,
+        _         => ' ',
+      };
+
+      let sub_tree = self.get_sub(first_char);
+      return sub_tree.put(key);
+    }
+    else
+    {
+      if self.terminal == false
+      {
+        self.terminal = true;
+        return true;
+      }
+      else { return false; }
+    }
+  }
+
+  pub fn print_all(&self)
+  {
+    println!("{}", self);
+    for elem in self.next.iter()
+    {
+      if let Some(sub) = elem
+      {
+        sub.print_all();
+      }
+    }
+  }
+
+  pub fn print_contents(&self)
+  {
+    self.print_contents_recur(true, &mut String::with_capacity(256));
+  }
+
+  fn print_contents_recur(&self, compact: bool, char_stack: &mut String)
+  {
+    if !self.root
+    {
+      char_stack.push(self.label);
+      if self.terminal {
+        if compact { print!("{} ", char_stack); }
+        else       { println!("{}", char_stack); }
+      }
+    }
+
+    for elem in self.next.iter()
+    {
+      if let Some(sub) = elem
+      {
+        sub.print_contents_recur(compact, char_stack);
+      }
+    }
+
+    if !self.root {
+      char_stack.pop();
+    }
+  }
+}
